a4095b8b8a5f75f802f4b44dd10ee2336bccdf60
Eduard Pruhlo <e.pruglo@absoluteweb.com>
2025-01-23 14:08:34 +0200

    feat(react-3): add props for post component

--- src/components/Profile/MyPosts.jsx
+++ src/components/Profile/MyPosts.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import NewPosts from "./NewPosts";
 import Post from "./Post";
 import './MyPosts.css';
+import PostAvatar from './../../post-avatar.png';
+import PostAvatarSecond from './../../post-avatar-2.png';
 
 const MyPosts = () => {
   return (
     <div className="posts">
       <NewPosts />
       <div className="posts__inner">
-        <Post />
-        <Post />
+        <Post name="Artur O." message="+ rep" avatar={PostAvatarSecond} />
+        <Post name="Maxim L." message="Hello!" avatar={PostAvatar} />
       </div>
     </div>
   );
