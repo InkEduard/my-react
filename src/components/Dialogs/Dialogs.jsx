c564669c33e33064c8ee94d2313cde13fa1f9b3d
Eduard Pruhlo <e.pruglo@absoluteweb.com>
2025-01-28 10:50:23 +0200

    feat(react-5): add map method for posts and messages

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -22,37 +22,23 @@ const Dialogs = () => {
     { id: '6', message: 'Hey'}
   ];
 
+  let dialogsElements = dialogsData.map( (dialog, i) => {
+    return <DialogItem key={i} id={dialog.id} name={dialog.name} />
+  });
+
+  let messagesElements = messageData.map( (element, i) => {
+    return <DialogMessage key={i} message={element.message} />
+  });
+
   return (
     <div className="dialogs">
       <div className="dialogs-items">
-        <DialogItem id={dialogsData[0].id} name={dialogsData[0].name} />
-        <DialogItem id={dialogsData[1].id} name={dialogsData[1].name} />
-        <DialogItem id="3" name="Vlad P."/>
-        <DialogItem id="4" name="Stanislav C."/>
-        <DialogItem id="5" name="Stasyan V."/>
-        <DialogItem id="6" name="Olegsii B."/>
+
+        {dialogsElements}
       </div>
       <div className="dialogs-messages">
-        {/* <Routes>
-          <Route path="dialogs/1" element={<DialogMessage message="I'm going to open a beer now!" />} />
-          <Route path="dialogs/2" element={<DialogMessage message="What are you doing?" />} />
-          <Route path="dialogs/3" element={<DialogMessage message="Yeeeep!" />} />
-          <Route path="dialogs/4" element={<DialogMessage message="Goodbye<" />} />
-          <Route path="dialogs/5" element={<DialogMessage message="Really?" />} />
-          <Route path="dialogs/6" element={<DialogMessage message="Hey" />} />
-        </Routes> */}
-        {/* <div className="dialogs-messages__item active">I'm going to open a beer now!</div>
-        <div className="dialogs-messages__item">What are you doing?</div>
-        <div className="dialogs-messages__item">Yeeeep!</div>
-        <div className="dialogs-messages__item">Goodbye</div>
-        <div className="dialogs-messages__item">Really?</div>
-        <div className="dialogs-messages__item">Hey</div> */}
-        <DialogMessage message={messageData[0].message} />
-        <DialogMessage message={messageData[1].message} />
-        <DialogMessage message="Yeeeep!" />
-        <DialogMessage message="Goodbye<" />
-        <DialogMessage message="Really?" />
-        <DialogMessage message="Hey" />
+
+        {messagesElements}
       </div>
     </div>
   );
